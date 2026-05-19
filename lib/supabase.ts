d158eb85b45e544ea871e8eda25b2c1267571e96
Valor Africa <safae12elouafi@gmail.com>
2026-05-19 19:36:39 +0100

    Update Supabase config

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://oglzwjtidfffqrvdqpca.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9nbHp3anRpZGZmZnFydmRxcGNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxNDIwMzQsImV4cCI6MjA5NDcxODAzNH0.JRUlpQvUjXRYX8P6fOez3QoHZYtp0EFNNQHpHQtF6Jo'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
